6a7eaa046a04418008d5d07acd33b194860b1816
dinesh11515 <dineshaitham2@gmail.com>
2022-06-12 21:58:19 +0530

    added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 module.exports = {
-  plugins: [
-    require('flowbite/plugin')
-],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
